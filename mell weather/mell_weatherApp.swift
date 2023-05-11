0e58289d61a4938f326213e63ee0314ae85a2ed5
BrunaMello <brunacrespomello@gmail.com>
2023-05-11 16:00:02 +0100

    Initial Commit

---
+++ mell weather/mell_weatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  mell_weatherApp.swift
+//  mell weather
+//
+//  Created by Bruna Mello on 11/05/2023.
+//
+
+import SwiftUI
+
+@main
+struct mell_weatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
